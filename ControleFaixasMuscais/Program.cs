cfcfe95f58be74717a9d1f7501f496de7d9fb24b
Denis <dennnis.projects@gmail.com>
2020-04-09 11:28:54 -0300

    versao incial

---
+++ ControleFaixasMuscais/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+
+        public const int KEYEVENTF_EXTENTEDKEY = 1;
+        public const int KEYEVENTF_KEYUP = 0;
+        public const int VK_MEDIA_NEXT_TRACK = 0xB0;// code to jump to next track
+        public const int VK_MEDIA_PLAY_PAUSE = 0xB3;// code to play or pause a song
+        public const int VK_MEDIA_PREV_TRACK = 0xB1;// code to jump to prev track
+
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 0)
+            {
+                if (args[0] == "PLAY_PAUSE")
+                {
+                    Console.WriteLine("Play ou Pause");
+                    // Play or Pause music
+                    keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+                }
+                else if (args[0] == "PREVIOUS")
+                {
+                    Console.WriteLine("Voltando");
+                    // Jump to previous track
+                    keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+                }
+                else
+                {
+                    Console.WriteLine("Indo para o proximo som...");
+                    // Jump to next track
+                    keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Indo para o proximo som...");
+                keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+            }
+            // Jump to previous track
+            // keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+            // Play or Pause music
+            // keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+            // Jump to next track
+            // keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+
+    }
+}
